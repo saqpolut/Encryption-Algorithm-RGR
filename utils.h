c217081b751c5a4a98fce869d026e712d8923cc4
axcelqq <alkolchikova@mail.ru>
2026-06-12 11:50:27 +0700

    удаление генератора случайного ключа для отдельной реализации

--- utils.h
+++ utils.h
@@ -8,15 +8,11 @@
 #define MAX_PATH_LEN 512
 #define BUFFER_SIZE 1024
 
-// функции работы со строками и вводом
 void clearInputBuffer();
 void removeNewline(char* str);
 int isEmpty(const char* str);
 void printSeparator();
 void waitForEnter();
 int strLength(const char* str);
-
-// Генератор случайного ключа
-void generateRandomKey(char* key, int maxLen);
 
 #endif
