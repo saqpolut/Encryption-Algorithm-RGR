3c835b78bff13cfbf4b0abe0124e9b9829a317f8
Admin <sergnat2007@gmail.com>
2026-06-13 14:33:03 +0700

    удаление дубликатов всех файлов, перемещенных в папки

--- KeyValidator_chiffre.cpp
+++
@@ -1,9 +0,0 @@
-#include "KeyValidator_chiffre.h"
-
-bool KeyValidator::isValid(const std::vector<uint8_t>& key, size_t minLen, size_t maxLen) {
-    return key.size() >= minLen && key.size() <= maxLen;
-}
-
-std::string KeyValidator::getErrorMessage() {
-    return "Key validation failed: length out of allowed range";
-}
